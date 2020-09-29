43852feabac64db8f4d58b4cc6eaeb66f3c2887b
Sergeq-mouse-code <moisecode99@gmail.com>
2020-09-29 21:25:09 +0300

    feat: add function reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let strNumber = n + '';
+    let result = '';
+    let lastNumber = '';
+    let length = strNumber.length;
+
+    if (strNumber[0] !== '-') lastNumber += strNumber[0];
+    for (let i = length - 1; i > 0; i--) {
+        result += strNumber[i];
+    }
+    return +(result + lastNumber);
 }
